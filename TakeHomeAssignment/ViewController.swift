ed5a0addb3a1df7e4b37ae6e835c28045f3bfa46
JenL1201 <jenyyy1201@gmail.com>
2023-08-11 12:53:13 -0700

    Initial Commit

---
+++ TakeHomeAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeHomeAssignment
+//
+//  Created by 卢小姐 on 8/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
