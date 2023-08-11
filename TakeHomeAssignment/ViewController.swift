84013596b8e8feb0d7a5a59b7908570f5033548b
JenL1201 <jenyyy1201@gmail.com>
2023-08-11 14:21:06 -0700

    Create TableView

--- TakeHomeAssignment/ViewController.swift
+++ TakeHomeAssignment/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Provide data from `TokenModel` for view controller
+    let tokenInfo = TokenModel()
 
+    // Create a table view
+    var tableView : UITableView {
+        let table = UITableView()
+        table.delegate = self
+        table.dataSource = self
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
+}
+
+// Handle row selection
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+// Provide data sources (number of rows and cells for each row)
+extension ViewController: UITableViewDataSource {
+    // Return number of tokens
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tokenInfo.list?.tokens.count ?? 0
+    }
+    
+    // Return cells for each given row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
 }
 
